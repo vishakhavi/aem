d4f5772f16eb3b96285327ab186c1079b8e4ce9f
vishakha vinayak <vinayak.v@northeastern.edu>
2022-11-13 02:59:22 -0500

    updating constants

--- core/src/main/java/com/anf/core/constants/AppConstants.java
+++ core/src/main/java/com/anf/core/constants/AppConstants.java
@@ -1,31 +1,9 @@
 package com.anf.core.constants;
 
 public class AppConstants {
-    public static final String FORWARD_SLASH = "/";
     public static final String EQUALS = "=";
-    public static final String NEW_LINE = "\n";
-
-    // TO-DO related constants
-    public static final String TODO_ENDPOINT = "https://jsonplaceholder.typicode.com/todos/";
-    public static final long TODO_THREAD_SLEEP_TIME = 14400000;
-
-    // Resource Resolver Factory sub-service
-    public static final String SUB_SERVICE = "tutorialSubService";
-
-    // Workflow Process Label
-    public static final String PROCESS_LABEL = "process.label";
-    // Workflow Chooser Label
-    public static final String CHOOSER_LABEL = "chooser.label";
-    // Child page count
-    public static final String CHILD_PAGE_COUNT = "childPageCount";
-
-    // User groups
-    public static final String ADMINISTRATORS = "administrators";
-    public static final String CONTENT_AUTHORS = "content-authors";
-
     // Dynamic datasource
     public static final String DATASOURCE = "datasource";
     public static final String DROPDOWN_SELECTOR = "dropdownSelector";
-    public static final String COUNTRY_LIST = "countryList";
     public static final String COUNTRY_LIST_PATH = "/content/dam/anf-code-challenge/exercise-1/countries.json";
 }
