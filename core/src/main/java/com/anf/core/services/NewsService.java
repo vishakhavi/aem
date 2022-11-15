d38f447afde0f57d7cccc03765e09f7c087280f4
vishakha vinayak <vinayak.v@northeastern.edu>
2022-11-14 22:19:27 -0500

    adding page policies

--- core/src/main/java/com/anf/core/services/NewsService.java
+++
@@ -1,9 +0,0 @@
-package com.anf.core.services;
-
-import java.util.List;
-import java.util.Map;
-
-public interface NewsService {
-	//public String createAuthorNode(String country, SlingHttpServletRequest request);
-	public void getNews(String path);
-}
